23048529411a40857a80a78c406d686b4c999b2d
Igbined <93369396+Igbined@users.noreply.github.com>
2026-04-07 12:13:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// Keep this external for security
-// config.js
 // config.js
 const CONFIG = {
-    DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1484291070106927104/rmZj_raRyYCQsZn4Fazz-hh0j6uuwrCDC18VMY-yOFtezFmEyt_7RkJuj9dZ8x5kwLDE"
+    DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1489579300687577279/fMVQanQ8kgUHZ9a_G39YhvpKjHnCmJmljDDHR9ntSa7bUAPMh1Axq0AK4VRumc10Npau"
 };
 
-export default CONFIG;
+// Make it available globally for both pages
+window.CONFIG = CONFIG;
