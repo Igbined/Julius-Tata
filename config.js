135ae305dc4429db5e4d1e7cc1fc0c5c72dc3f71
Igbined <93369396+Igbined@users.noreply.github.com>
2026-04-28 18:21:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-    DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1497736058216120391/Sv3Vi-3lx833q_yMNVA0g2VIdrRKUzkn6e9HQ72pIvxplYPXrY3gg-QFzWbLe8SM70D2"
+    DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1498735883698966659/NRNn0JL5cuPLFRZZEeJsTlmB5PMWTF8Skb6rqBfaceswQNgKpfopuoSz1Ha74b6nVeL8"
 };
 
 // Make it available globally for both pages
