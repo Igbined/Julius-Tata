73c97f2ffae3cf64a07f8fb52c5d241be2ff2b84
Igbined <93369396+Igbined@users.noreply.github.com>
2026-05-02 22:33:05 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const CONFIG = {
-    DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1497736058216120391/Sv3Vi-3lx833q_yMNVA0g2VIdrRKUzkn6e9HQ72pIvxplYPXrY3gg-QFzWbLe8SM70D2"
-};
-
-// Make it available globally for both pages
-window.CONFIG = CONFIG;
